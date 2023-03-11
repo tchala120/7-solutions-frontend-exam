1811ff66c57c304a9ff3653487fec3cd94f5379a
Panupong Tipjoi <itstoon.panupong@gmail.com>
2023-03-12 01:45:56 +0700

    feat: Added configuration for variables with search limit and skip

--- src/react-query/useListProducts.ts
+++ src/react-query/useListProducts.ts
@@ -1,22 +1,61 @@
-import { AxiosError } from 'axios'
+import type { AxiosError } from 'axios'
 
-import { useQuery } from 'react-query'
+import queryString from 'query-string'
+import {
+  useQuery,
+  type UseQueryResult,
+  type UseQueryOptions,
+} from 'react-query'
 
-import { Product, type ReactQueryUseQueryHook } from 'src/react-query/types'
+import { pagination } from './request'
+
+import type { Product } from 'src/react-query/types'
+
+interface ListProductsVariables {
+  search?: string
+  limit?: number
+  skip?: number
+}
+
+interface ListProductsQueryOptions
+  extends UseQueryOptions<ListProductsResponse, AxiosError> {
+  variables?: ListProductsVariables
+}
 
 interface ListProductsResponse {
+  limit: number
+  total: number
+  skip: number
   products: Product[]
 }
 
-const useListProducts: ReactQueryUseQueryHook<
-  ListProductsResponse,
-  AxiosError
-> = (options) =>
-  useQuery<ListProductsResponse, AxiosError>('/products?limit=100', {
+const useListProducts = (
+  options?: ListProductsQueryOptions
+): UseQueryResult<ListProductsResponse, AxiosError> => {
+  const search = options?.variables?.search
+  const limit = options?.variables?.limit
+  const skip = options?.variables?.skip
+
+  const listItemQueryString = queryString.stringify({
+    limit: limit || pagination.limit,
+    skip,
+  })
+
+  const listItemSearchQueryString = queryString.stringify({
+    q: search,
+  })
+
+  const path =
+    search == null || search === ''
+      ? `/products?${listItemQueryString}`
+      : `/products/search?${listItemSearchQueryString}`
+
+  return useQuery<ListProductsResponse, AxiosError>(path, {
     onError(error) {
       console.log('Error from list products query', error)
     },
     ...(options ?? {}),
   })
+}
 
 export default useListProducts
