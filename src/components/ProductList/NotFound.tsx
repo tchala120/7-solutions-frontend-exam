df8b45bdbc9d9ba95f73dd406e54146c7fa9474b
Panupong Tipjoi <itstoon.panupong@gmail.com>
2023-03-12 01:44:27 +0700

    feat: Added product not found component

---
+++ src/components/ProductList/NotFound.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+const ProductNotFound = styled.div`
+  width: 100%;
+  height: 50vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  font-size: 36px;
+  font-weight: bold;
+`
+
+export default ProductNotFound
