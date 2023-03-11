f3414d1bf945429c805fb3308bd878d5807b724c
Panupong Tipjoi <itstoon.panupong@gmail.com>
2023-03-11 22:56:28 +0700

    feat: Setup storage

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,10 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
+import setupStorage from './services/localStorage'
+
+setupStorage()
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
