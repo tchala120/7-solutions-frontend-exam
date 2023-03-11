77efe98967e313fd1446651ed1213e301fb6cc21
Panupong Tipjoi <itstoon.panupong@gmail.com>
2023-03-11 07:42:41 +0700

    feat: Added home page

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import { Button } from 'antd'
+
+const HomePage = () => {
+  return (
+    <>
+      <h1>This is home page</h1>
+
+      <Button>Hello world</Button>
+    </>
+  )
+}
+
+export default HomePage
