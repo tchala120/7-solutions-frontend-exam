43fd8c562942425ca5f9ed4fe946f9f1c29b4921
Panupong Tipjoi <itstoon.panupong@gmail.com>
2023-03-12 01:44:41 +0700

    feat: Added product list component

---
+++ src/components/ProductList/ProductList.tsx
@@ -0,0 +1,112 @@
+import { Col, Row, Skeleton } from 'antd'
+import { useState } from 'react'
+import InfiniteScroll from 'react-infinite-scroll-component'
+
+import ProductListControl from './Control'
+import ProductListItem from './ListItem'
+import SkeletonLoading from './SkeletonLoading'
+
+import useListProducts from 'src/react-query/useListProducts'
+
+import type { Product } from 'src/react-query/types'
+
+const listProductLimit = 10
+
+const ProductList = () => {
+  const { data, pagination, shouldLoadMoreContent, onSearch, onNextLoad } =
+    useProductList()
+
+  return (
+    <InfiniteScroll
+      dataLength={pagination.total}
+      next={onNextLoad}
+      hasMore={shouldLoadMoreContent}
+      loader={
+        <Row gutter={[16, 16]} style={{ marginTop: 16 }}>
+          <Col md={12} xs={24}>
+            <Skeleton.Input
+              style={{
+                height: 130,
+              }}
+              active
+              block
+            />
+          </Col>
+          <Col span={12}>
+            <Skeleton.Input
+              style={{
+                height: 130,
+              }}
+              active
+              block
+            />
+          </Col>
+        </Row>
+      }
+    >
+      <Row gutter={[16, 16]}>
+        <ProductListControl
+          searchResultTotalItems={pagination.total}
+          onSearch={onSearch}
+        />
+
+        {data == null ? (
+          <SkeletonLoading />
+        ) : (
+          <ProductListItem listProducts={data} />
+        )}
+      </Row>
+    </InfiniteScroll>
+  )
+}
+
+export default ProductList
+
+const useProductList = () => {
+  const [searchKeyword, setSearchKeyword] = useState<string>()
+  const [skip, setSkip] = useState(0)
+
+  const [listProducts, setListProducts] = useState<Product[]>()
+
+  const listProductsQuery = useListProducts({
+    variables: {
+      search: searchKeyword,
+      limit: listProductLimit,
+      skip,
+    },
+    onSuccess(data) {
+      setListProducts((prev) =>
+        Array.isArray(prev) ? [...prev, ...data.products] : data.products
+      )
+    },
+  })
+
+  const total = listProductsQuery.data?.total || 0
+
+  const changeToFirstPage = () => {
+    setSkip(0)
+    setListProducts(undefined)
+  }
+
+  return {
+    loading: listProductsQuery.isLoading,
+    query: listProductsQuery,
+    data: listProducts,
+    searchKeyword,
+    pagination: {
+      skip,
+      total,
+    },
+    shouldLoadMoreContent:
+      searchKeyword == null || (searchKeyword === '' && skip < total),
+    changeToFirstPage,
+    onSearch(value: string) {
+      changeToFirstPage()
+
+      setSearchKeyword(value)
+    },
+    onNextLoad() {
+      setSkip((prev) => prev + listProductLimit)
+    },
+  }
+}
