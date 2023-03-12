0151dc9b289791c91110db94c99d4509e02e7605
Panupong Tipjoi <itstoon.panupong@gmail.com>
2023-03-13 00:19:41 +0700

    feat: Added routeTo helper for generate path with params and additional query string

---
+++ src/helpers/utils.ts
@@ -0,0 +1,15 @@
+import queryString, { type ParsedQuery } from 'query-string'
+import { generatePath } from 'react-router-dom'
+
+interface RouteToOptions {
+  params?: Record<string, string | number>
+  query?: ParsedQuery
+}
+
+export const routeTo = (originalPath: string, options?: RouteToOptions) => {
+  const path = generatePath(originalPath, options?.params)
+
+  return options?.query == null
+    ? path
+    : `${path}?${queryString.stringify(options.query)}`
+}
